0c8116a68d90c8069e1723d92cf022162f3bf31f
Aaron Sanchez <saarong12@gmail.com>
2025-11-07 00:41:09 -0600

    Added more of a breakdown for the auth flow with PKCE.

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ import (
 )
 
 // TODO: Add Auth with PKCE to get Spotify access token.
+// 1: Create a code verifier
+// 2: Create a code challenge from the verifier (SHA256)
+// 3: Request user authorization with code challenge
+// 4: Upon successful authorization, get the code from the callback url
+// 5: Take aforementioned code and request an access token using the code verifier from the step 1
+// 6: Upon successful token creation, use the token to request user data from Spotify
+
 // TODO: Create a player and request current state of the users playback.
 
 func main() {
